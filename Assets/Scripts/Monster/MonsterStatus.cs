7ac5d904d382051cf54f162a67132abd91f47ed5
GreedSpirit <dlwjdgus0918@naver.com>
2025-12-12 16:12:08 +0900

    [Feat] Monster Status Base
    테이블로 데이터를 읽기 전 기초적인 Status에 필요한 데이터 및 임시 UI 세팅

---
+++ Assets/Scripts/Monster/MonsterStatus.cs
@@ -0,0 +1,82 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MonsterGrade
+{
+    Common,
+    Boss
+}
+
+public class MonsterStatus : MonoBehaviour
+{
+    [SerializeField] private string _monsterName;
+    [SerializeField] private string _monsterLevel;
+    [SerializeField] private MonsterGrade _monsterGrade;
+    [SerializeField] private int _monsterMaxHP;
+    [SerializeField] private int _monsterCurHP;
+    [SerializeField] private int _monsterDefense;
+
+    private List<IMonsterHpObserver> _hpObservers = new List<IMonsterHpObserver>(); //옵저버 목록을 관리할 List
+
+    public MonsterStatus(string name, string level, MonsterGrade grade, int maxHp, int defense)
+    {
+        //! 추후 데이터 테이블에서 읽어와서 초기화 할 때 할당
+        _monsterName = name;
+        _monsterLevel = level;
+        _monsterGrade = grade;
+        _monsterMaxHP = maxHp;
+        _monsterCurHP = maxHp;
+        _monsterDefense = defense;
+    }
+
+    private void Start()
+    {
+        _monsterCurHP = _monsterMaxHP;
+        NotifyHpObservers(); //초기 HP 상태를 알림
+    }
+
+    public void TakeDamage(int damage)
+    {
+        //이 곳에 damage 계산 로직 추가(방어력, 보호막 등)
+        _monsterCurHP -= damage;
+        if (_monsterCurHP < 0)
+            _monsterCurHP = 0;
+        NotifyHpObservers(); //HP 변경 알림
+
+        //몬스터 죽음 관련 로직 추가
+    }
+
+    public void AddHpObserver(IMonsterHpObserver observer)
+    {
+        if (!_hpObservers.Contains(observer)) //방어 코드
+        {
+            _hpObservers.Add(observer);
+        }
+    }
+
+    public void RemoveHpObserver(IMonsterHpObserver observer)
+    {
+        if (_hpObservers.Contains(observer)) //방어 코드
+        {
+            _hpObservers.Remove(observer);
+        }
+    }
+
+    private void NotifyHpObservers() //옵저버들에게 HP 변경 알림
+    {
+        foreach (var observer in _hpObservers)
+        {
+            observer.OnMonsterHpChanged(_monsterCurHP, _monsterMaxHP);
+        }
+    }
+
+
+    //! 유니티 에디터를 사용할 때 체력 소모 이벤트에 대한 테스트를 위한 코드 추후 삭제 필요
+    #if UNITY_EDITOR
+    public void TestTakeDamage()
+    {
+        TakeDamage(10);
+    }
+    #endif
+
+}
---
+++ Assets/Scripts/Monster/Observer/UIMonsterHp.cs
@@ -0,0 +1,27 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMonsterHp : MonoBehaviour, IMonsterHpObserver
+{
+    [SerializeField] private Slider _monsterHpSlider;
+    [SerializeField] private TextMeshProUGUI _monsterHpText;
+    [SerializeField] private MonsterStatus _monsterStatus;
+
+    private void Start()
+    {
+        _monsterStatus.AddHpObserver(this);
+    }
+
+    private void OnDestroy()
+    {
+        _monsterStatus.RemoveHpObserver(this);
+    }
+
+    public void OnMonsterHpChanged(int currentHp, int maxHp)
+    {
+        _monsterHpSlider.maxValue = maxHp;
+        _monsterHpSlider.value = currentHp;
+        _monsterHpText.text = $"{currentHp} / {maxHp}"; // 추후 표시 상황 변경 가능
+    }
+}
