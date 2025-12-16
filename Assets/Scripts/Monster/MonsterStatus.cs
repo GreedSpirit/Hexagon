8e3f7435923a82aaee00e59877564de999130251
GreedSpirit <dlwjdgus0918@naver.com>
2025-12-16 17:09:02 +0900

    [Feat] Monster TakeDamage Logic Change
    기획서 받는 기본 피해량 기본 공식에 따른 피해량 로직 수정

--- Assets/Scripts/Monster/MonsterStatus.cs
+++ Assets/Scripts/Monster/MonsterStatus.cs
@@ -7,15 +7,19 @@ using UnityEngine;
 
 public class MonsterStatus : MonoBehaviour
 {
+    [SerializeField] private int _monsterId; //몬스터 데이터 ID
     [SerializeField] private int _monsterLevel; //추후 스테이지 테이블에서 불러올 내용
     [SerializeField] private MonsterGrade _monsterGrade;
     [SerializeField] private int _monsterMaxHP;
     [SerializeField] private int _monsterCurHP;
+    public int MonsterCurHP => _monsterCurHP; // 외부에서 몬스터의 현재 체력에 접근할 수 있는 프로퍼티
     [SerializeField] private int _monsterDefense;
     [SerializeField] private int _monsterShield = 0;
-    [SerializeField] private MonsterData _monsterStatData;
+    [SerializeField] private float _monsterBuff = 0;
+    [SerializeField] private float _monsterDebuff = 0;
+    [SerializeField] private MonsterData _monsterData;
     [SerializeField] private MonsterSkillSetData _monsterSkillSet;
-    [SerializeField] private MonsterStatData _monsterStat;
+    [SerializeField] private MonsterStatData _monsterStatData;
 
     private List<IMonsterHpObserver> _hpObservers = new List<IMonsterHpObserver>(); //옵저버 목록을 관리할 List
 
@@ -24,33 +28,33 @@ public class MonsterStatus : MonoBehaviour
     public void InitMonsterStatus()
     {
         //추후 어떻게 사용할지에 따라서 중복 변수이기 때문에 DataManager에서 불러오는 방식으로 변경할 수도 있음
-        _monsterGrade = _monsterStatData.MonGrade;
+        _monsterGrade = _monsterData.MonGrade;
 
         if(_monsterGrade == MonsterGrade.Normal)
         {
             _monsterLevel = 3; //추후 스테이지 관련 테이블에서 갖고와서 레벨 설정하기
-            _monsterStat = DataManager.Instance.GetCommonMonsterStatData(_monsterLevel);
+            _monsterStatData = DataManager.Instance.GetCommonMonsterStatData(_monsterLevel);
 
-            _monsterMaxHP = Mathf.FloorToInt(_monsterStat.Hp * _monsterStatData.HpRate); 
-            _monsterDefense = Mathf.FloorToInt(_monsterStat.Defense * _monsterStatData.DefRate);
+            _monsterMaxHP = Mathf.FloorToInt(_monsterStatData.Hp * _monsterData.HpRate); 
+            _monsterDefense = Mathf.FloorToInt(_monsterStatData.Defense * _monsterData.DefRate);
         }
         else if(_monsterGrade == MonsterGrade.Boss)
         {
             _monsterLevel = 1; //추후 스테이지 관련 테이블에서 갖고와서 레벨 설정하기
             // 500은 임시 기본 체력 수치, 추후 _monsterLevel 값을 이용해서 BossMonsterStat 테이블에서 수치 갖고오기
-            _monsterMaxHP = Mathf.FloorToInt(500 * _monsterStatData.HpRate);
-            _monsterDefense = Mathf.FloorToInt(10 * _monsterStatData.DefRate);
+            _monsterMaxHP = Mathf.FloorToInt(500 * _monsterData.HpRate);
+            _monsterDefense = Mathf.FloorToInt(10 * _monsterData.DefRate);
         }
-        _monsterSkillSet = DataManager.Instance.GetMonsterSkillSetData(_monsterStatData.SkillSet);
+        _monsterSkillSet = DataManager.Instance.GetMonsterSkillSetData(_monsterData.SkillSet);
         _monsterCurHP = _monsterMaxHP;        
     }
 
     private void Start()
     {
         // ID 방식 Key 방식 구별
-        _monsterStatData = DataManager.Instance.GetMonsterStatData(1);
+        _monsterData = DataManager.Instance.GetMonsterStatData(1);
         //_monsterStatData = DataManager.Instance.GetMonsterStatData("KeyMonsterGhost0001");
-        if(_monsterStatData == null)
+        if(_monsterData == null)
         {
             Debug.LogError("몬스터 스탯 데이터를 불러오지 못했습니다. Id: 1");
             return;
@@ -61,18 +65,11 @@ public class MonsterStatus : MonoBehaviour
             InitMonsterStatus();
             NotifyHpObservers(); //초기 HP 상태 갱신
         }
-        //바로 아래는 아직 CSV 파일이 없어서 MonsterSkillSetData에서 스킬셋 데이터를 불러오는 코드 주석 처리
-        //_monsterSkillSet = DataManager.Instance.GetMonsterSkillSetData(_monsterStatData.SkillSet);
-        //if(_monsterSkillSet == null)
-        //{
-        //    Debug.LogError("몬스터 스킬셋 데이터를 불러오지 못했습니다. Key: " + _monsterStatData.SkillSet);
-        //    return;
-        //}
     }
 
     public void TakeDamage(int damage) // 데미지를 입을 때 호출할 함수
     {
-        damage = Mathf.Max(damage -  Mathf.Min(_monsterDefense, 7), 0); //방어력 적용(방어력 최대가 7이라서 7까지만 적용)
+        damage = (int)Mathf.Max((damage -  Mathf.Min(_monsterDefense, 7)) * (1 + _monsterDebuff), 0); //방어력 적용(방어력 최대가 7이라서 7까지만 적용)
         if(_monsterShield > 0)
         {
             int shieldDamage = Mathf.Min(_monsterShield, damage);
