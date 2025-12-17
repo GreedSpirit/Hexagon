338a28f85a14076d067e135fb5aa3d224e2a7f76
Songmin00 <dn7272@gmail.com>
2025-12-17 09:46:07 +0900

    [Feat] IBattleUnit interface
    전투 참여 유닛이 공통으로 상속할 IBattleUnit 인터페이스 정의.

---
+++ Assets/Scripts/BattleSystem_SMS/IBattleUnit.cs
@@ -0,0 +1,10 @@
+public interface IBattleUnit
+{
+    public void TakeDamage(int damage);
+
+    public void TakeTrueDamage(int damage);
+
+    public void GetHp(int hp);
+
+    public void GetShield(int shield);
+}
