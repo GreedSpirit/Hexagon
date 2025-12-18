18a563ecdb88cc9f8b43008155916f34c1425490
Songmin00 <dn7272@gmail.com>
2025-12-18 15:30:31 +0900

    [Feat] ShieldUI, DefenseUI
    플레이어 보호막 및 방어력을 나타내는 UI 추가 및 테스트 Scene 연결.

--- Assets/Scripts/Character_Songmin/CharacterUI/PlayerUIManager.cs
+++ Assets/Scripts/Character_Songmin/CharacterUI/PlayerUIManager.cs
@@ -5,6 +5,8 @@ public class PlayerUIManager : MonoBehaviour
     [SerializeField] GameObject _playerExpBar;
     [SerializeField] GameObject _playerHpBar;
     [SerializeField] GameObject _playerHpText;    
+    [SerializeField] GameObject _playerDefenseText;    
+    [SerializeField] GameObject _playerShieldText;    
     [SerializeField] GameObject _playerLevelText;    
 
     private void Start()
@@ -19,6 +21,8 @@ public class PlayerUIManager : MonoBehaviour
             _playerExpBar.SetActive(true);
             _playerHpBar.SetActive(true);
             _playerHpText.SetActive(true);
+            _playerDefenseText.SetActive(true);
+            _playerShieldText.SetActive(true);
             _playerLevelText.SetActive(true);
         }
         else
@@ -26,6 +30,8 @@ public class PlayerUIManager : MonoBehaviour
             _playerExpBar.SetActive(false);
             _playerHpBar.SetActive(false);
             _playerHpText.SetActive(false);
+            _playerDefenseText.SetActive(false);
+            _playerShieldText.SetActive(false);
             _playerLevelText.SetActive(false);
         }            
     }        
