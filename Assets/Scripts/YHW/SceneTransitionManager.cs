2f7b9cd86d795efb9c405ad23763448756a6737b
hyunWoong <gusdnd3724@naver.com>
2025-12-19 16:48:55 +0900

    [feat] 씬전환 , 싱글톤 스크립트 생성

---
+++ Assets/Scripts/YHW/SceneTransitionManager.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+public class SceneTransitionManager : MonoBehaviour
+{
+    public static SceneTransitionManager Instance { get; private set; }
+
+    [SerializeField] private Image fadeImage;  
+    [SerializeField] private float fadeDuration = 1f; 
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // 씬 전환에도 유지
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        if (fadeImage != null)
+        {
+            // 시작 시 페이드 인
+            StartCoroutine(FadeIn());
+        }
+    }
+
+    /// <summary>
+    /// 페이드 씬 전환
+    /// </summary>
+    public void LoadSceneWithFade(string sceneName)
+    {
+        StartCoroutine(FadeOutAndLoad(sceneName));
+    }
+
+    private IEnumerator FadeOutAndLoad(string sceneName)
+    {
+        // 페이드 아웃
+        float t = 0;
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            SetAlpha(t / fadeDuration);
+            yield return null;
+        }
+        SetAlpha(1);      
+        SceneManager.LoadScene(sceneName);
+        yield return StartCoroutine(FadeIn());
+    }
+
+    private IEnumerator FadeIn()
+    {
+        float t = fadeDuration;
+        while (t > 0)
+        {
+            t -= Time.deltaTime;
+            SetAlpha(t / fadeDuration);
+            yield return null;
+        }
+        SetAlpha(0);
+    }
+
+    private void SetAlpha(float alpha)
+    {
+        if (fadeImage != null)
+        {
+            Color c = fadeImage.color;
+            c.a = alpha;
+            fadeImage.color = c;
+        }
+    }
+}
