15f25465c851f67a7422e136eaedf8dd318f9e7c
Songmin00 <dn7272@gmail.com>
2025-12-23 16:35:58 +0900

    [Feat] DungeonEntrance
    던전 입구에 닿으면 던전 선택창이 뜨도록 클래스 추가.

---
+++ Assets/Scripts/Character_Songmin/Village/DungeonEntrance.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class DungeonEntrance : MonoBehaviour
+{
+    [SerializeField] DungeonPresenter _presenter;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Player player))
+        {
+            _presenter.ShowDungeon();
+        }
+    }    
+}
