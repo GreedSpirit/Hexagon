41bbb338a9c9aa12a6804645051d9b8543046045
Songmin00 <dn7272@gmail.com>
2025-12-26 17:30:17 +0900

    [Fix] TalkUI
    토크가 끝나지 않던 오류 수정.

--- Assets/Scripts/Character_Songmin/CharacterUI/TalkUI.cs
+++ Assets/Scripts/Character_Songmin/CharacterUI/TalkUI.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,20 +13,26 @@ public class TalkUI : MonoBehaviour
     Coroutine _fadeRoutine;    
     float fadeDuration = 0.3f;
     ITalkable _currentTalking;
-
-    //
+    List<ScenarioData> _currentScenario;
+    int _currentIndex;
+    
     [SerializeField] GameObject _talkPannel;
-    [SerializeField] Text _img;
+    [SerializeField] Image _backgroundPannel;
+    [SerializeField] Image _leftImg;
+    [SerializeField] Image _rightImg;
     [SerializeField] TextMeshProUGUI _characterName;
     [SerializeField] TextMeshProUGUI _characterScript;
     [SerializeField] GameObject _upgradeEnterButton;
     [SerializeField] GameObject _upgradePannel;
     [SerializeField] GameObject _testButton;
     
+    
+
 
     private void Awake()
     {
         _canvasGroup = _fadePannel.GetComponent<CanvasGroup>();
+        _currentScenario = new List<ScenarioData>();
     }
 
     private void Start()
@@ -34,10 +41,13 @@ public class TalkUI : MonoBehaviour
         _testButton.SetActive(false);
     }
 
-    public void EnterTalk(ITalkable talkable)
+    //마을에서 대화용
+    //------------------------------------------------------
+    public void EnterTalk(ITalkable talkable) 
     {
         SetTalkable(talkable);
-        _currentTalking = talkable;        
+        _currentTalking = talkable; 
+        _rightImg.gameObject.SetActive(false);
         if (_fadeRoutine != null)
         {
             StopCoroutine(_fadeRoutine);
@@ -73,26 +83,78 @@ public class TalkUI : MonoBehaviour
         {
             _upgradePannel.SetActive(false);
             _testButton.SetActive(false);
-        }
-        
+        }        
     }
 
+    public void EnterScenario(List<ScenarioData> datas)
+    {
+        if (_fadeRoutine != null)
+        {
+            StopCoroutine(_fadeRoutine);
+        }
+        _currentScenario = datas;
+        _currentIndex = 0;
+        //_fadeRoutine = //여기서 해야할 일 : 까만 화면 만들기
+        UpdateScenario();
+    }
 
-
-
+    public void UpdateScenario()
+    {
+        ScenarioData data = _currentScenario[_currentIndex];
+        //_backgroundPannel.sprite = 스프라이트 설정해주기
+        //만약에 까만 화면이면 페이드 아웃 해주기
+        if (_leftImg.sprite == null && data.Character_Image == "Img_PC_Erion")
+        {
+            //_leftImg.sprite = ImageManager.Instance.GetSprite(data.Character_Image);
+            if (_rightImg.gameObject.activeSelf)
+            {
+                _rightImg.gameObject.GetComponent<CanvasGroup>().alpha = 0.5f;
+            }
+            _leftImg.gameObject.SetActive(true);
+            _leftImg.gameObject.GetComponent<CanvasGroup>().alpha = 1f;
+            _characterName.text = data.Npc;            
+        }
+        else if (_rightImg.sprite == null)
+        {
+            //_rightImg.sprite = ImageManager.Instance.GetSprite(data.Character_Image);
+            if (_leftImg.gameObject.activeSelf)
+            {
+                _leftImg.gameObject.GetComponent<CanvasGroup>().alpha = 0.5f;
+            }
+            _rightImg.gameObject.SetActive(true);
+            _rightImg.gameObject.GetComponent<CanvasGroup>().alpha = 1f;
+            _characterName.text = data.Npc;            
+        }
+        else if (data.Character_Image == null)
+        {
+            _characterName.text = "";
+        }
+        _characterScript.text = data.Dialogue;
+        _currentIndex++;
+        //연출도 추가해주기
+    }
 
 
 
 
 
     //-------------------------------------
-    
+    private void SetScenario()
+    {
+
+    }
+
+
     private void SetTalkable(ITalkable talkable)
     {
         _characterName.text =  talkable.GetName();
         _characterScript.text =  talkable.GetTalk();
-        _img.text = talkable.GetImage();
+        //_leftImg.GetComponent<Text>().text = talkable.GetImage();
     }
+
+    
+    
+
 
 
     IEnumerator FadeInAndOut(bool isEnter)
