e7400b763c090f83fd29a1e0168f91136608a228
Songmin00 <dn7272@gmail.com>
2025-12-30 07:18:30 +0900

    [Feat] PlayerLevelUpUI, PlayerLevelUpObject
    플레이어 레벨업 시 띄울 팝업 UI 제작.

---
+++ Assets/Scripts/Character_Songmin/CharacterUI/StatusUI/PlayerLevelUpUI.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerLevelUpUI : MonoBehaviour
+{
+    [SerializeField] GameObject _levelUpUIPrefab;    
+
+    void Start()
+    {
+        if (Player.Instance != null)
+        {
+            Player.Instance.OnLevelChanged += ShowLevelUpUI;
+        }            
+    }
+
+    private void OnDestroy()
+    {
+        if (Player.Instance != null)
+        {
+            Player.Instance.OnLevelChanged -= ShowLevelUpUI;
+        }            
+    }
+
+    void ShowLevelUpUI(int level)
+    {
+        PlayerLevelUpObject popup =  Instantiate(_levelUpUIPrefab, transform).GetComponent<PlayerLevelUpObject>();
+        popup.Init(level);
+    }
+}
