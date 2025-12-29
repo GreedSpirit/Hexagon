8d1c27499b8f6b82e1c998f3ec65cfa0193ed7a3
Songmin00 <dn7272@gmail.com>
2025-12-29 12:46:30 +0900

    [Fix] StatusEffectIcon
    상태이상 효과 적용 아이콘이 이미지와 연동되도록 변경.

--- Assets/Scripts/Character_Songmin/CharacterUI/StatusUI/StatusEffectIcon.cs
+++ Assets/Scripts/Character_Songmin/CharacterUI/StatusUI/StatusEffectIcon.cs
@@ -4,14 +4,14 @@ using UnityEngine.UI;
 
 public class StatusEffectIcon : MonoBehaviour
 {
-    [SerializeField] Text _iconImage; // 테스트용 설정, 추후 이미지 추가되면 Image로 변경 후 이미지 불러오는 로직 추가.
+    [SerializeField] Image _iconImage; // 테스트용 설정, 추후 이미지 추가되면 Image로 변경 후 이미지 불러오는 로직 추가.
     [SerializeField] TextMeshProUGUI _turnText;
     StatusEffectData _data;
 
     public void Init(StatusEffectData data)
     {
         _data = data;
-        _iconImage.text = _data.Img;
+        _iconImage.sprite = DataManager.Instance.GetSprite(SpriteType.Status , _data.Img);
     }
 
     public void UpdateIcon(int turn)
