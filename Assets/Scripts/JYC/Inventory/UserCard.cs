78eb5c9d18d6e423bdfa4d42306cb6edb551da64
thaud0000 <thaud3967@gmail.com>
2025-12-30 15:45:02 +0900

    [Fix] User Card
    유저 카드 최신화

--- Assets/Scripts/JYC/Inventory/UserCard.cs
+++ Assets/Scripts/JYC/Inventory/UserCard.cs
@@ -1,17 +1,33 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
-// 유저가 실제로 보유한 카드 정보
 [System.Serializable]
-public class UserCard
+public class UserCard : ISerializationCallbackReceiver 
 {
     public int InstanceId;      // 고유 식별자 (필요시)
     public int CardId;          // 데이터 테이블 ID 참조
     public int Level;           // 카드 레벨
     public int Count;           // 보유 개수 (최대 99)
-    public DateTime AcquiredTime; // 획득 시간 (정렬용)
+    public DateTime AcquiredTime;
+
+    [HideInInspector] public long AcquiredTimeTicks;
 
     public CardData GetData() => DataManager.Instance.GetCard(CardId);
+
+    public void OnBeforeSerialize()
+    {
+        AcquiredTimeTicks = AcquiredTime.Ticks;
+    }
+
+    public void OnAfterDeserialize()
+    {
+        try
+        {
+            AcquiredTime = new DateTime(AcquiredTimeTicks);
+        }
+        catch
+        {
+            AcquiredTime = DateTime.Now; // 에러 나면 현재 시간으로
+        }
+    }
 }
